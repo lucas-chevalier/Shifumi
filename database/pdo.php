19caa5459e7d3ec24b9e5f0558f32a302a28c9f8
Kevmorlo <kevinlebeau2004@gmail.com>
2023-01-13 00:49:09 +0100

    Grosse MAJ

--- database/pdo.php
+++ database/pdo.php
@@ -1,7 +1,13 @@
 <?php
-require(".env");
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 $options = [
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'", "PDO::ERRMODE_EXCEPTION"
   ];
+$hostname = getenv("hostname");
+$base_de_donnees = getenv("base_de_donnees");
+$user = getenv("user");
+$password = getenv("password");
 $dsn = "mysql:host=$hostname;dbname=$base_de_donnees";
 $dbh = new PDO($dsn, $user, $password, $options);
--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-require(".env");
+require __DIR__ ."database/.env";
 $options = [
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'", "PDO::ERRMODE_EXCEPTION"
   ];
