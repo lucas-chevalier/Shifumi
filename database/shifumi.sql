e9107463af9e54686b00bc688523b974722e459c
Lucas-Chevalier <115011849+Lucas-Chevalier@users.noreply.github.com>
2023-01-19 14:43:17 +0100

    Rajout des valeurs par défaut de la bdd

--- database/shifumi.sql
+++ database/shifumi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 19 jan. 2023 à 11:36
+-- Généré le : jeu. 19 jan. 2023 à 13:42
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -32,12 +32,12 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(45) COLLATE utf8mb4_bin NOT NULL,
   `date_de_creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `score_utilisateur` int(7) NOT NULL,
-  `score_hal` int(7) NOT NULL,
-  `nombre_parties` int(7) NOT NULL,
+  `score_utilisateur` int(7) NOT NULL DEFAULT '0',
+  `score_hal` int(7) NOT NULL DEFAULT '0',
+  `nombre_parties` int(7) NOT NULL DEFAULT '0',
   `adresse_ip` varchar(45) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
